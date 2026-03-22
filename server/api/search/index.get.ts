c697b9c368d6f77cdbe4f57159c4a2d88280bf82
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-22 16:33:25 -0600

    Update index.get.ts

--- server/api/search/index.get.ts
+++ server/api/search/index.get.ts
@@ -1,21 +1,13 @@
 import { searchAnime } from "animeflv-scraper";
 
 export default defineEventHandler(async (event) => {
-  // 🌐 CORS
   setHeader(event, "Access-Control-Allow-Origin", "*");
   setHeader(event, "Access-Control-Allow-Methods", "GET,OPTIONS");
   setHeader(event, "Access-Control-Allow-Headers", "Content-Type, x-api-key");
 
-  // 🔥 PREFLIGHT
-  if (event.method === "OPTIONS") {
-    return {
-      status: 200
-    };
-  }
+  if (event.method === "OPTIONS") return "";
 
-  // 🔐 API KEY
   const apiKey = getHeader(event, "x-api-key");
-
   const envKey =
     process.env.API_KEY ||
     event.context.cloudflare?.env?.API_KEY;
@@ -24,25 +16,12 @@ export default defineEventHandler(async (event) => {
     throw createError({ statusCode: 401 });
   }
 
-  const { query, page } = getQuery(event) as { query: string, page: string };
+  const { query } = getQuery(event);
 
-  const base = await searchAnime(query, Number(page) || 1);
-
-  if (!base || !base.media?.length) {
-    throw createError({ statusCode: 404 });
-  }
-
-  const data = base.media.map((a: any) => ({
-    title: a.title,
-    cover: a.cover,
-    slug: a.slug,
-    rating: a.rating,
-    type: a.type
-  }));
+  const res = await searchAnime(query as string, 1);
 
   return {
     success: true,
-    total: data.length,
-    data
+    data: res?.media || []
   };
 });
