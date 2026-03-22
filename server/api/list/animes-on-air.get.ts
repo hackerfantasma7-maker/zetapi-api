0a20cd8ec328e18ecec32f24745d6b4adf617326
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-22 14:37:59 -0600

    Update animes-on-air.get.ts

--- server/api/list/animes-on-air.get.ts
+++ server/api/list/animes-on-air.get.ts
@@ -1,6 +1,19 @@
 import { getOnAir } from "animeflv-scraper";
 
 export default defineEventHandler(async (event) => {
+  // 🌐 CORS
+  setHeader(event, "Access-Control-Allow-Origin", "*");
+  setHeader(event, "Access-Control-Allow-Methods", "GET,OPTIONS");
+  setHeader(event, "Access-Control-Allow-Headers", "Content-Type, x-api-key");
+
+  // 🔥 PREFLIGHT
+  if (event.method === "OPTIONS") {
+    return {
+      status: 200
+    };
+  }
+
+  // 🔐 API KEY
   const apiKey = getHeader(event, "x-api-key");
 
   const envKey =
@@ -14,12 +27,6 @@ export default defineEventHandler(async (event) => {
       data: { success: false, error: "Invalid API KEY" }
     });
   }
-
-  setHeader(event, "Access-Control-Allow-Origin", "*");
-  setHeader(event, "Access-Control-Allow-Methods", "GET,OPTIONS");
-  setHeader(event, "Access-Control-Allow-Headers", "*");
-
-  if (event.method === "OPTIONS") return;
 
   const onair = await getOnAir();
 
