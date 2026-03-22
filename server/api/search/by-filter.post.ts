b79763c67ff9ea0274df3f29a7e9ed4d313a619d
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-21 19:13:12 -0600

    Add CORS headers to search filter endpoint

--- server/api/search/by-filter.post.ts
+++ server/api/search/by-filter.post.ts
@@ -1,5 +1,9 @@
 export default defineEventHandler(async (event) => {
 
+  setResponseHeaders(event, {
+    "Access-Control-Allow-Origin": "*",
+  });
+
   const body = await readBody(event);
 
   return {
