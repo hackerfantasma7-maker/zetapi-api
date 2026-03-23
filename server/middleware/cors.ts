9cdeecf75c9ad4d2362dbafc4fe37097a8c40849
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-22 21:07:20 -0600

    Create cors.ts

---
+++ server/middleware/cors.ts
@@ -0,0 +1,10 @@
+export default defineEventHandler((event) => {
+  setHeader(event, "Access-Control-Allow-Origin", "*");
+  setHeader(event, "Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  setHeader(event, "Access-Control-Allow-Headers", "Content-Type, x-api-key");
+
+  if (event.method === "OPTIONS") {
+    setResponseStatus(event, 200);
+    return "";
+  }
+});
