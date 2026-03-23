1039909a6b6d636c3de71755a49806ac5b5287f3
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-22 21:04:07 -0600

    Update latest-episodes.get.ts

--- server/api/list/latest-episodes.get.ts
+++ server/api/list/latest-episodes.get.ts
@@ -1,8 +1,12 @@
 import { getLatest } from "animeflv-scraper";
 
 export default defineEventHandler(async (event) => {
+ // 🔥 CORS FIX
   setHeader(event, "Access-Control-Allow-Origin", "*");
+  setHeader(event, "Access-Control-Allow-Methods", "GET,OPTIONS");
+  setHeader(event, "Access-Control-Allow-Headers", "Content-Type, x-api-key");
 
+  if (event.method === "OPTIONS") return "";
   const latest = await getLatest();
 
   if (!latest) {
