bb021ee545f74429d8c44ab25272cc8debb386a6
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-22 23:34:39 -0600

    Update filter.ts

--- server/utils/filter.ts
+++ server/utils/filter.ts
@@ -1,3 +1,4 @@
 export async function filterWorkingServers(servers: any[]) {
+  // 🔥 NO validar con fetch directo (rompe embeds)
   return servers.filter(s => s.embed && s.embed.startsWith("http"));
 }
