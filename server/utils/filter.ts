b16b9ced63b72f11fd2a43b9c208e11d01f355b2
samvelzeta <160423772+samvelzeta@users.noreply.github.com>
2026-03-22 22:06:18 -0600

    Update filter.ts nuevo

--- server/utils/filter.ts
+++ server/utils/filter.ts
@@ -2,7 +2,7 @@ export async function filterWorkingServers(servers: any[]) {
   const results = await Promise.all(
     servers.map(async (s) => {
       try {
-        await fetch(s.embed, { method: "HEAD" });
+        await fetch(s.embed, { method: "GET" });
         return s;
       } catch {
         return null;
